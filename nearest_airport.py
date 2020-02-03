3a713c9e65726a016c9b924a92cc41c19f685491
sujithnpoojary01 <57342810+sujithnpoojary01@users.noreply.github.com>
2020-02-03 16:00:05 +0530

    Update nearest_airport.py

--- nearest_airport.py
+++ nearest_airport.py
@@ -117,4 +117,4 @@ def main():
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='127.0.0.1', port=5000, debug=False)
